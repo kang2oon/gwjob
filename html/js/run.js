347ceb594c496d021081d610f3d90798403c0d67
aldo814 <iko_hello@naver.com>
2021-07-29 17:21:51 +0900

    -

--- html/js/run.js
+++ html/js/run.js
@@ -845,37 +845,6 @@ $(document).ready(function () {
 
 })
 
-jQuery(function($) {
-		var $frame = $('.tab_slider');
-	var $slidee = $frame.children('ul').eq(0);
-	var $wrap = $frame.parent();
-
-	// Call Sly on frame
-	$frame.sly({
-		horizontal: 1,
-		itemNav: 'basic',
-		smart: 1,
-		activateOn: 'click',
-		mouseDragging: 1,
-		touchDragging: 1,
-		releaseSwing: 1,
-		startAt: 0,
-		scrollBy: 1,
-		activatePageOn: 'click',
-		speed: 300,
-		elasticBounds: 1,
-		easing: 'easeOutExpo',
-		dragHandle: 1,
-		dynamicHandle: 1,
-		clickBar: 1,
-
-		// Buttons
-		prevPage: $wrap.parent().find('.prev'),
-		nextPage: $wrap.parent().find('.next')
-	});
-
-});
-
 
 function openModal(modalname) {
 	document.get
