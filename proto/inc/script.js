c000f138ccbb9eab6cca8da688966195a8a81378
kang2oon <kang2oon@gmail.com>
2022-08-19 12:37:31 +0900

    연혁 반응형 스타일 추가

--- proto/inc/script.js
+++ proto/inc/script.js
@@ -11,7 +11,31 @@ $(document).ready(function(){
             $('.mod_history02 .history').slick({
                 slidesToShow: 3,
                 slidesToScroll: 1,
-                arrows: true
+                arrows: true,
+                infinite: false,
+                adaptiveHeight: true,
+                responsive: [
+                    {
+                        breakpoint: 1025,
+                        settings: {
+                            arrows: false,
+                            }
+                    },
+                    {
+                        breakpoint: 821,
+                        settings: {
+                            slidesToShow: 2,
+                            arrows: false,
+                            }
+                    },
+                    {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        arrows: false,
+                        }
+                    }
+                ]
             });
         }
     });
